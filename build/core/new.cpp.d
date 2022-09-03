6beaf36f1e5e1998024a3e64529bc5d254b1d808
KeithLin7024 <keithlin724@gmail.com>
2022-09-03 17:01:01 +0800

    delete build file

--- build/core/new.cpp.d
+++
@@ -1,2 +0,0 @@
-d:\Arduino\Library\KYSevenSegment\build\core\new.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\new.cpp
--- build/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-d:\Arduino\Library\KYSevenSegment\build\core\wiring_pulse.S.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring_pulse.S
